7e7922e866f7fac6f4365b28cbc5ee5fffebcff9
kevin <mattisonii@yahoo.com>
2016-06-17 14:20:55 -0700

    Remove jsOff class for non-JS users and add jsOn class.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
-$(document).ready(function() {
-  $('body').append('<p>Made with jQuery</p>');
+$(document).read(function() {
+  ///remove 'jsOff' class from the body, and add 'jsOn' class
+  ///for users who have JS enabled
+  $('body').removeClass('jsOff').addClass('jsOn');
 });
