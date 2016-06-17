ad3273da40c71a0fed21322f588a50f063e7ac48
kevin <mattisonii@yahoo.com>
2016-06-17 14:23:22 -0700

    Hide Tabbed content using css-display: none

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(document).read(function() {
+$(document).ready(function() {
   ///remove 'jsOff' class from the body, and add 'jsOn' class
   ///for users who have JS enabled
   $('body').removeClass('jsOff').addClass('jsOn');
