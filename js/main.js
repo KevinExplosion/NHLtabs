a8b32a52b3bd6a58c707246c7431eed96bc40727
kevin <mattisonii@yahoo.com>
2016-06-29 17:12:39 -0700

    Add class for currently selected tab, and activate selected link.

--- js/main.js
+++ js/main.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
   ///remove 'jsOff' class from the body, and add 'jsOn' class
   ///for users who have JS enabled
   $('body').removeClass('jsOff').addClass('jsOn');
+
+  $('#tabs a').on('click', function(e) {
+    $('#tabs a.current').removeClass('current');
+    $('.tab-section:visible').hide();
+    $(this.hash).show();
+    $(this).addClass('current');
+    e.preventDefault();
+  });
 });
